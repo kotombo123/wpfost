6324f45f5a2780b88a869e02d760c1a0f39035f9
kotombo123 <01199327@pw.edu.pl>
2026-05-12 15:16:35 +0200

    Add Book class with properties for a book

---
+++ Book.cs
@@ -0,0 +1,10 @@
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Genre { get; set; }
+    public int Year { get; set; }
+    public string Publisher { get; set; }
+    public byte[] Cover { get; set; } // Do konwertera obrazów
+}
